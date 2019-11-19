a855d803038d63a19f5cdc60128b11c5e74274b2
lihongbo6668201 <lihongbo6668201@126.com>
2019-11-19 19:12:49 +0800

    修改版

--- practice/py/ssq/ssqtj.py
+++ practice/py/ssq/ssqtj.py
@@ -17,6 +17,7 @@ if not os.path.exists(path):
     os.makedirs(path)
 
 # 初始化结果Excel
+filename = path + 'ssq_new_' + time.strftime('%Y%m%d%H%M%S', time.localtime()) + '.xlsx'
 resFile = openpyxl.Workbook()
 sheet = resFile.active
 sheet.title = "历史双色球统计结果"
@@ -46,8 +47,8 @@ soap = bs4.BeautifulSoup(res.text.encode('iso-8859-1'), "html.parser")
 sfy = soap.select('span div a')
 print('共' + str(len(sfy)) + '期' )
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"}
-head={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
+headers1 = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"}
+headers2={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
 
 # 循环所有分页
 for idx in range(len(sfy)):
@@ -55,15 +56,20 @@ for idx in range(len(sfy)):
     redlist = ''
     suburl = sfy[idx].get('href')
     subtxt = sfy[idx].getText()
-
-    if subtxt != '17001':
-        continue
-
     logging.info(suburl)
-    subres = requests.get(suburl, headers=head)
+
+    # 防止服务器反爬主动断开，每次换不同的
+    if idx % 2 == 0:
+        head = headers2
+    else:
+        head = headers1
+    time.sleep(0.1)
+
+    # 子页面爬取
+    subres = requests.get(suburl, head)
     subres.raise_for_status()
 
-    # 解析子页面
+# 解析子页面
     subsoap = bs4.BeautifulSoup(subres.text.encode('iso-8859-1'), "html.parser")
     sublist = subsoap.select('tr td div ul li')
 
@@ -72,9 +78,9 @@ for idx in range(len(sfy)):
         # 红号
         red_no = sublist[subidx].getText()
         redlist += sublist[subidx].getText() + ' '
-        print(red_no)
+        # print(red_no)
         if red_no.isdigit() is True:
-            print(len(red_no))
+            # print(len(red_no))
             red_idx = int(red_no)
             sheet['B' + str(red_idx + 1)].value += 1
 
@@ -85,7 +91,8 @@ for idx in range(len(sfy)):
         sheet['D' + str(blue_idx + 1)].value += 1
 
     result = "第" + sfy[idx].getText() + "期：" + redlist + '| ' + blue_no
-    print(result)
+    # print(result)
     sheet['E' + str(idx + 2)] = result
+    resFile.save(filename)
 
-resFile.save(path + '\\' + 'ssq_new_'  + time.strftime('%Y%m%d%H%M%S', time.localtime()) + '.xlsx')
+resFile.save(filename)
